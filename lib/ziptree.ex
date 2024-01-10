62ee29038daefb482e55fc5488e55af05d39c694
VitorTrin <emailreservadovitor@gmail.com>
2024-01-10 14:48:18 -0300

    Add ex doc

--- lib/ziptree.ex
+++ lib/ziptree.ex
@@ -1,7 +1,17 @@
 defmodule Ziptree do
   @moduledoc """
-  Documentation for `Ziptree`.
+  ## What is a zip tree
+  Zip tree is a random access data structure that is equivalent to a skiplist,
+  but instead of being stored in multiple arrays it is stored in a binary balanced tree.
+
+  It was created in 2018 (Zip Trees, by Robert E. Tarjan, Caleb C. Levy, Stephen Timmel) and then futher improved
+  in 2023 (Zip-zip Trees: Making Zip Trees More Balanced, Biased, Compact, or Persistent by Ofek Gila, Michael T. Goodrich, Robert E. Tarjan)
+
+  ## What is this lib
+
+  This is a nif for the rust implementation of zip trees, more specifically the zip zip tree implementation.
   """
+
   alias Ziptree.Nif
 
   @type ziptree :: reference()
